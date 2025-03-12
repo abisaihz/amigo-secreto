caa5003ed788b4c1e43eb5bb0c9960e6cdff0e67
Abisai Hernandez <ajhernande@outlook.com>
2025-03-11 20:23:36 -0700

    Agrega funcion para agregar

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+// Empezamos creando el array para los amigos que se van a capturar
+let amigos = [];
+
+
+function sortearAmigo() {
+
+
+}
+
+
+
+function agregarAmigo() {
+    let textoIngresado = document.getElementById("amigo");
+    amigos.push(textoIngresado);
+    return;
+}
+
+
+console.log(amigos);
