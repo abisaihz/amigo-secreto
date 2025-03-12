7570935911f234d29443a8a4cdbb71077bbc470a
Abisai Hernandez <ajhernande@outlook.com>
2025-03-12 08:40:05 -0700

    Finaliza js con funciones

--- app.js
+++ app.js
@@ -2,14 +2,25 @@
 
 
 // Empezamos creando el array para los amigos que se van a capturar
-let amigos = [];
-
+let amigos;
+iniciarPagina()
 
 function sortearAmigo() {
 if (amigos.length < 2) {
     alert("No seria un misterio, por favor ingrese mas de un amigo");
+} else {
+    let numAmigo = Math.floor(Math.random()*amigos.length);
+    selAmigo = amigos[numAmigo];
+    // Aqui se imprime para probar el resultado
+    console.log(`Amigo seleccionado ${selAmigo}`);
+    editarEtiqueta("resultado",`Amigo seleccionado: ${selAmigo}`);
 }
 
+}
+
+function iniciarPagina() {
+    amigos = [];
+    editarEtiqueta("listaAmigos", "");
 }
 
 
@@ -21,10 +32,28 @@ function agregarAmigo() {
         amigos.push(textoIngresado);    
         // Aqui se imprime la consola para pruebas
         console.log(amigos);
+        limpiarInput("amigo")
+        agregarLista(textoIngresado);
         return;
     }
     alert("Por favor ingrese un nombre en el campo editable");
 }
 
 
-console.log(amigos);
+function agregarLista(amigoSeleccionado) {
+    let ul = document.getElementById("listaAmigos");
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(amigoSeleccionado));
+    ul.appendChild(li);
+    return;
+}
+
+function editarEtiqueta(idHtml, texto) {
+    let title = document.getElementById(idHtml);
+    title.innerHTML = texto;
+    return;
+}
+
+function limpiarInput(idHtml) {
+    document.getElementById(idHtml).value = "";
+}
