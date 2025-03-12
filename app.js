8f2252a7515258fe34f7473b3f4c524f4b1952dc
Abisai Hernandez <ajhernande@outlook.com>
2025-03-11 20:39:38 -0700

    Inician las dos funciones de js

--- app.js
+++ app.js
@@ -6,16 +6,24 @@ let amigos = [];
 
 
 function sortearAmigo() {
-
+if (amigos.length < 2) {
+    alert("No seria un misterio, por favor ingrese mas de un amigo");
+}
 
 }
 
 
+// Esto es lo que ocurre cuando se presiona el boton de agregar amigo
 
 function agregarAmigo() {
-    let textoIngresado = document.getElementById("amigo");
-    amigos.push(textoIngresado);
-    return;
+    let textoIngresado = document.getElementById("amigo").value;
+    if (textoIngresado != "") {
+        amigos.push(textoIngresado);    
+        // Aqui se imprime la consola para pruebas
+        console.log(amigos);
+        return;
+    }
+    alert("Por favor ingrese un nombre en el campo editable");
 }
 
 
